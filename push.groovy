bc98f9d7e01440ba0a84db957c5e93432a8716bd
0marSater <omar.buis22@gmail.com>
2023-04-23 00:28:30 +0200

    .

--- push.groovy
+++ push.groovy
@@ -12,8 +12,8 @@
 def pushImage(){
     docker.withRegistry('https://815919245801.dkr.ecr.eu-west-3.amazonaws.com/flask-app', 'ecr:eu-west-3:aws-ecr-credentials'){
         echo "Start pushing image $IMAGE_NAME ..."
-        sh "docker tag $IMAGE_NAME $REPO_NAME:$IMAGE_NAME"
-        sh "docker push $REPO_NAME:$IMAGE_NAME"
+        sh "docker tag $IMAGE_NAME $REPO_NAME:latest"
+        sh "docker push $REPO_NAME:latest"
     }
 }
 
