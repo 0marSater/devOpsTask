27ebeb35e93efbf472951618b419f73bbdd778fe
0marSater <omar.buis22@gmail.com>
2023-05-04 17:43:06 +0300

    create jenkins folder

--- Jenkins/push.groovy
+++ Jenkins/push.groovy
@@ -1,7 +1,7 @@
 def pushImage(){
     echo "Login to ecr ..."
     // replace <account-id> with your account id, and <database-name> with your ECR name.
-    docker.withRegistry('https://<account-id>.dkr.ecr.eu-west-3.amazonaws.com/<ECR-name>',
+    docker.withRegistry('https://<account-id>.dkr.ecr.<region-code>.amazonaws.com/<ECR-name>',
                             'ecr:<region-code>:<aws-credentials>'){
 
         echo "Start pushing image $IMAGE_NAME ..."
