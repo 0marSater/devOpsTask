5f273fdc001da77b3678d78597d5338360accc62
0marSater <111258496+0marSater@users.noreply.github.com>
2023-05-11 21:47:15 +0300

    Update push.groovy

--- Jenkins/push.groovy
+++ Jenkins/push.groovy
@@ -3,9 +3,7 @@ def pushImage(){
     // replace <account-id> with your account id, and <ECR-name> with your ECR name.
     docker.withRegistry('https://<account-id>.dkr.ecr.<region-code>.amazonaws.com/<ECR-name>',
                             'ecr:<region-code>:<aws-credentials>'){
-
         echo "Start pushing image $IMAGE_NAME ..."
-        tag
         sh "docker tag $IMAGE_NAME $REPO_NAME:latest"
         sh "docker push $REPO_NAME:latest"
     }
