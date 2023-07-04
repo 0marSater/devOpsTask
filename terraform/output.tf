309a2c55d4166f7d00fd46fc5c3067df5cd57ef1
0marSater <111258496+0marSater@users.noreply.github.com>
2023-07-04 18:38:56 +0300

    update output.tf root file

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,19 +1,3 @@
-#vpc
-output "azs" {
-  description = "name of azs"
-  value       = module.VPC.azs
-}
-
-output "vpc_id" {
-  description = "name of vpc"
-  value       = module.VPC.vpc_id
-}
-
-output "private_subnets" {
-  description = "name of vpc"
-  value       = module.VPC.private_subnets
-}
-
 #rds
 output "db_instance_address" {
   description = "The address of the RDS instance"
@@ -27,32 +11,9 @@ output "cluster_name" {
 }
 
 
-output "cluster_oidc_issuer_url" {
-  description = "The cluster oidc issuer url"
-  value       = module.EKS.cluster_oidc_issuer_url
-}
-
-
-output "oidc_provider" {
-  description = "The cluster oidc provider"
-  value       = module.EKS.oidc_provider
-}
-
-output "cluster_endpoint" {
-  description = "The cluster cluster endpoint"
-  value       = module.EKS.cluster_endpoint
-}
-
-
-output "cluster_certificate_authority_data" {
-  description = "The cluster certificate authority data"
-  value       = module.EKS.cluster_certificate_authority_data
-}
-
 #ecr
 output "repository_url" {
   description = "The URL of the repository"
   value       = module.ECR.repository_url
 }
 
-#alb
