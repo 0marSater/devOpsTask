45a308ebed3fcce253212ce08d53b5683ca3b0e4
0marSater <111258496+0marSater@users.noreply.github.com>
2023-07-04 18:37:59 +0300

    update push.groovy

--- push.groovy
+++ push.groovy
@@ -1,8 +1,9 @@
 def pushImage(){
     echo "Login to ecr ..."
-    // replace <account-id> with your account id, and <ECR-name> with your ECR name.
-    docker.withRegistry('https://815919245801.dkr.ecr.eu-west-2.amazonaws.com/task-repo',
-                            'ecr:eu-west-2:aws-credentials'){
+    // replace <account-id> with your account id, <region> with your region, <repo-name> with your repository name,
+   // and <jenkins_aws_credentials> with ur credentials for iam jenkins on aws
+    docker.withRegistry('https://<account-id>.dkr.ecr.<region>.amazonaws.com/<repo-name>',
+                            'ecr:<region>:<jenkins_aws_credentials>'){
         echo "Start pushing image $IMAGE_NAME ..."
         sh "docker tag $IMAGE_NAME $REPO_NAME:latest"
         sh "docker push $REPO_NAME:latest"
